bcbdda485c56d55217923d9ffdb7012346b19b78
akramhany <akramhany65@gmail.com>
2026-06-26 18:36:31 +0300

    chore(nws): remove some tests

--- src/services/preprocessing/orchestrator.py
+++ src/services/preprocessing/orchestrator.py
@@ -47,7 +47,8 @@ def preprocess(input: PreprocessingInput) -> PreprocessingOutput:
 
     # 2. Word Boundary Detection
     completed_prefix, current_fragment, mode = split_word_boundary(
-        normalized_text, None # no support for current_cursor for now
+        normalized_text,
+        None,  # no support for current_cursor for now
     )
 
     # 3. Segmentation (completed prefix only)
